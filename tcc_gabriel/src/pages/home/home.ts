cbc3ac365844bdc67f868567515569cace9c7187
Oliver Henrique <it.oliverhm@gmail.com>
2017-03-14 00:13:40 -0300

    Adiciona a home e a página de compra e venda.

--- tcc_gabriel/src/pages/home/home.ts
+++ tcc_gabriel/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +8,54 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private actionSheetCtrl: ActionSheetController) {
 
   }
 
+
+	btnActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'O QUE VOCÊ DESEJA FAZER:',
+      buttons: [
+        {
+          text: 'SOLICITAR CAÇAMBA',
+          role: 'solicitar-cacamba',
+          handler: () => {
+            console.log('SOLICITAR CAÇAMBA clicked');
+            // this.navCtrl.parent.select(2);
+          }
+        },
+        {
+          text: 'RETIRAR CAÇAMBA',
+          role: 'retirar-cacamba',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },
+        {
+          text: 'SOLICITAR ACESSORIA',
+          role: 'solicitar-acessoria',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },
+        {
+          text: 'COMPRA | VENDA | TROCA',
+          role: 'compra-venda-troca',
+          handler: () => {
+            console.log('VENDAS clicked');
+            this.navCtrl.parent.select(1);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
 }
