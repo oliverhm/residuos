1f2b1e5e17e1ed6543799d5cd7dd32a80cd961bd
Oliver Henrique Maceira <oliver@ciandt.com>
2017-08-31 20:55:15 -0300

    Mais alterações do Gabriel.

--- tcc_gabriel/src/app/app.module.ts
+++ tcc_gabriel/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { CadastroPagePage } from "../pages/cadastro/cadastro";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    CadastroPagePage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -23,7 +25,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    CadastroPagePage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- tcc_gabriel/src/pages/contact/contact.ts
+++ tcc_gabriel/src/pages/contact/contact.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { CadastroPagePage } from "../cadastro/cadastro";
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  opened: boolean = false;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  navCadastroPage() {
+    this.navCtrl.push(CadastroPagePage);
+  }
+
+  openRadio() {
+    this.opened = true;
+  }
 }
